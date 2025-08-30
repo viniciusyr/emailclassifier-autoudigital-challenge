f603199df1cbee1113af3aa25232779a9ee96ec7
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-08-30 13:35:15 +0800

    frontend/feature: result

--- frontend/app/components/homepage.tsx
+++ frontend/app/components/homepage.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react';
 import UploadForm from '../components/UploadEmail';
+import Result from '../components/Result';
 
 export default function HomePage() {
       const [result, setResult] = useState<{ Categoria: string; Resposta: string } | null>(null);
@@ -9,7 +10,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6">Email Classifier</h1>
       <UploadForm onResult={setResult} />
-      {/* {result && <ResultCard categoria={result.Categoria} resposta={result.Resposta} />} */}
+       {result && <Result category={result.Categoria} response={result.Resposta} />}
     </div>
   );
 }
