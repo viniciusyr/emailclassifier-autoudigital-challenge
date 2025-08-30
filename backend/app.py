7c1f555d6e53dc62ff59a5ffaa5962f38204f534
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-08-30 16:51:57 +0800

    Update read-email to async await

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
+from importlib.metadata import files
+from pydoc import text
 from fastapi import FastAPI, UploadFile, Form
-import json
 from classifier import classify_email
 from process_email import process_email
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from split_emails import split_emails
 from starlette.responses import StreamingResponse
+import json
 
 
 
@@ -35,20 +37,18 @@ async def read_email_json(input: EmailInput):
 
 @app.post("/read")
 async def read_email(files: list[UploadFile] = None, text: str = Form(None)):
+    contents = []
+
+    if files:
+        for file in files:
+            raw = (await file.read()).decode("utf-8", errors="ignore")
+            contents.append(raw)
+    elif text:
+        contents.append(text)
+    else:
+        return {"error": "Invalid data"}
+
     async def event_stream():
-        contents = []
-
-        if files:
-            for file in files:
-                raw = (await file.read()).decode("utf-8", errors="ignore")
-                contents.append(raw)
-        elif text:
-            contents.append(text)
-        else:
-            yield json.dumps({"error": "Invalid data"}) + "\n"
-            return
-
-
         for raw in contents:
             for email in split_emails(raw):
                 clean_text = process_email(email)
