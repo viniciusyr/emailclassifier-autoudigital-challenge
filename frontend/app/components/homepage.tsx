bb2814e0e1373aa97c5c8aa8df02422ff3491f07
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-08-30 13:02:07 +0800

    feature: initial home page

---
+++ frontend/app/components/homepage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-6">Email Classifier</h1>
+    </div>
+  );
+}
